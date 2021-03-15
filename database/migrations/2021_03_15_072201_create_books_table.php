44c56ddea7880d2d6e0acb34ecc875a44830eb12
Olga Fursova <olga.fursova.zp@gmail.com>
2021-03-15 14:37:10 +0200

    vue SPA, created Books resource

--- database/migrations/2021_03_15_072201_create_books_table.php
+++ database/migrations/2021_03_15_072201_create_books_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCakesTable extends Migration
+class CreateBooksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateCakesTable extends Migration
      */
     public function up()
     {
-        Schema::create('cakes', function (Blueprint $table) {
+        Schema::create('books', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ class CreateCakesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('cakes');
+        Schema::dropIfExists('books');
     }
 }
